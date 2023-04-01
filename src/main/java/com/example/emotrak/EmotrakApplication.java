a0a5b4b8a8ba3f8ca67eb3b41f16c5c4d179302b
bambee <daj0909@naver.com>
2023-04-01 17:48:37 +0900

    feat : initial commit

---
+++ src/main/java/com/example/emotrak/EmotrakApplication.java
@@ -0,0 +1,13 @@
+package com.example.emotrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotrakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmotrakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/emotrak/EmotrakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emotrak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotrakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
